eac889f2601d1326c3ec6f8a74731984d8760ba9
artem.nekrasov <artem.nekrasov@moonfare.com>
2022-09-28 23:19:58 +0200

    Факториал + Валидация имени

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,27 @@
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  if (n === 0 || n === 1) {
+    return (1);
+  }
+  while (n > 1) {
+    n--;
+    result = result * n;
+  }
+  return result;
 }
+
+/*
+
+// второй способ
+
+function faclorial_for(n) {
+    if (n === 0 || n === 1) {
+        return (1);
+    }
+    for (let i = n - 1; i >= 1; i--) {
+        n = n * i;
+    }
+    return n;
+}
+
+*/
