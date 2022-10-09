081b53b0d00f8ab0bd9e713d163c9b4381e6207e
artem.nekrasov <artem.nekrasov@moonfare.com>
2022-10-09 21:51:55 +0200

    Строка в camelСase

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
-  // ваш код...
+  const arr = str.split('-');
+
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  return arr.join('');
 }
