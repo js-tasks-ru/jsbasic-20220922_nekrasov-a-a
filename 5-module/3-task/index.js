acdf3d1c31eeb298c9931f5f12f715a57576bc96
artem.nekrasov <artem.nekrasov@moonfare.com>
2022-10-18 18:58:30 +0200

    Задачи Занятие 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  const body = document.body;
+
+  const rightArrow = body.querySelector('.carousel__arrow_right');
+  const leftArrow = body.querySelector('.carousel__arrow_left');
+  const carousel = body.querySelector('.carousel__inner');
+  const carChildWidth = carousel.querySelector('.carousel__slide').offsetWidth;
+  const elementsCount = carousel.querySelectorAll('.carousel__slide').length;
+  const carWidth = carChildWidth * elementsCount;
+
+  let offset = 0;
+
+  rightArrow.style.display = 'none';
+
+  rightArrow.addEventListener('click', () => {
+    offset -= carChildWidth;
+    carousel.style.transform = `translateX(-${offset}px)`;
+
+
+    if (offset <= 0) {
+      rightArrow.style.display = 'none';
+    } else {
+      leftArrow.style.display = '';
+    }
+  });
+
+  leftArrow.addEventListener('click', () => {
+    offset += carChildWidth;
+    carousel.style.transform = `translateX(-${offset}px)`;
+
+    if (offset >= (carWidth - carChildWidth)) {
+      leftArrow.style.display = 'none';
+    } else {
+      rightArrow.style.display = '';
+    }
+  });
 }
